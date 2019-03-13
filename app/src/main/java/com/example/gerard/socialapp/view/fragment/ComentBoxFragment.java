7b1e8e28d4ea3daffcbd6e0c423682d96acbe232
Adri <agonzalez97>
2019-03-13 03:22:26 +0100

    SocialApp

---
+++ app/src/main/java/com/example/gerard/socialapp/view/fragment/ComentBoxFragment.java
@@ -0,0 +1,37 @@
+package com.example.gerard.socialapp.view.fragment;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gerard.socialapp.R;
+
+public class ComentBoxFragment extends Fragment {
+
+    private ComentBoxViewModel mViewModel;
+
+    public static ComentBoxFragment newInstance() {
+        return new ComentBoxFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.coment_box_fragment, container, false);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ComentBoxViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/gerard/socialapp/view/fragment/ComentBoxViewModel.java
@@ -0,0 +1,7 @@
+package com.example.gerard.socialapp.view.fragment;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ComentBoxViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
